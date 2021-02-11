a380b3e29878be7e6b9a3a73835aa24c1296d965
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2021-02-11 16:51:07 +0300

    init commit after generate lib

---
+++ src/lib/minimalistic-ui-kit-for-angular.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MinimalisticUiKitForAngularComponent } from './minimalistic-ui-kit-for-angular.component';
+
+
+
+@NgModule({
+  declarations: [MinimalisticUiKitForAngularComponent],
+  imports: [
+  ],
+  exports: [MinimalisticUiKitForAngularComponent]
+})
+export class MinimalisticUiKitForAngularModule { }
